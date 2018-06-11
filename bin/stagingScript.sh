5fae650f9419bd45b0a794eb6cebc6ab0b8e31c0
Jesse Dillman <jesse@thinkboxcreative.com>
2018-06-11 11:04:36 -0400

    build process

---
+++ bin/stagingScript.sh
@@ -0,0 +1,20 @@
+echo "Installing Git deployment";
+cd $WEBROOT;
+mkdir $STAGING.git;
+mkdir build;
+cd $STAGING.git;
+git init --bare;
+cd hooks;
+touch post-receive;
+echo "#!/bin/sh
+unset GIT_DIR;
+export GIT_WORK_TREE=$WEBROOT/build;
+export GIT_DIR=$WEBROOT/$STAGING.git;
+git checkout -f master;
+cd $WEBROOT/build && bundle exec jekyll build --destination $PUBLIC;
+rsync -r $WEBROOT/build/* $PUBLIC/wp-content/themes/grif" >> post-receive;
+
+echo "Setting up deployments";
+cd $WEBROOT/$STAGING.git/hooks;
+chmod 755 post-receive;
+echo "Complete";

b19dbd6f2a46ca7b62fc331965fe3c4e898c45bd
Jesse Dillman <jesse@thinkboxcreative.com>
2018-06-11 14:09:40 -0400

    fix deploy process

--- bin/stagingScript.sh
+++ bin/stagingScript.sh
@@ -11,8 +11,7 @@ unset GIT_DIR;
 export GIT_WORK_TREE=$WEBROOT/build;
 export GIT_DIR=$WEBROOT/$STAGING.git;
 git checkout -f master;
-cd $WEBROOT/build && bundle exec jekyll build --destination $PUBLIC;
-rsync -r $WEBROOT/build/* $PUBLIC/wp-content/themes/grif" >> post-receive;
+cd $WEBROOT/build && bundle exec jekyll build --destination $PUBLIC;" >> post-receive;
 
 echo "Setting up deployments";
 cd $WEBROOT/$STAGING.git/hooks;
